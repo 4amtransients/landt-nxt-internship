24cf42aec7b739a2b02f6af5a164ca0f363e5fb7
Avanti Bhandarkar <51116892+avantibhandarkar@users.noreply.github.com>
2020-05-26 01:12:13 +0530

    Update glass-knn.py

--- misc/glass-knn.py
+++ misc/glass-knn.py
@@ -13,6 +13,8 @@ X = df.drop('Type',axis=1).values
 y = df['Type'].values
 h = 0.2
 
+#K-nearest neighbour model
+
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25)
 
@@ -35,3 +37,22 @@ plt.legend()
 plt.xlabel('Number of neighbors')
 plt.ylabel('Accuracy')
 plt.show()
+
+knn = KNeighborsClassifier(n_neighbors=10)
+knn.fit(X_train,y_train)
+knn.score(X_test,y_test)
+
+from sklearn.metrics import confusion_matrix
+
+y_predict = knn.predict(X_test)
+confusion_matrix(y_test,y_predict)
+#or use the code below
+pd.crosstab(y_test, y_predict, rownames=['True'], colnames=['Predicted'], margins=True)
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test,y_predict,zero_division=1))
+
+#boxplot for the refractive index and class of glass
+
+sns.boxplot('Type', 'RI', data = df)
+
